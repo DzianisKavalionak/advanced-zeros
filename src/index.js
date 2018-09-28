796784295fd1729c62f6f731f1f79af5312ac8be
DzianisKavalionak <nereidy4@gmail.com>
2018-09-28 17:11:27 +0300

    65/100

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 module.exports = function getZerosCount(number, base) {
-
     let zeros = 0;
     let n = 0;
     let a = n;
+    let array = [];
     for (let n = 1; n < base; n++){
         if (base % n === 0) {
-
             a = n;
+            array.push(a);
         }
     }
-
-    if (a != 1) {
+    if (a !== 1 && Math.pow(array[1], array.length - 1) !== array[array.length - 1]) {
         for (let i = a; number / i >= 1; i) {
             zeros += Math.floor(number / i);
             i = i * a;
         }
-    } else if (a == 1){
+    } else if (a === 1){
             for (let i = base; number / i >= 1; i) {
-            zeros += Math.floor(number / i)  ;
-            i = i * base;
+                zeros += Math.floor(number / i)  ;
+                i = i * base;
+            }
+    } else if  (Math.pow(array[1], array.length - 1) === array[array.length - 1]){
+            for (let i = array[1]; number / i >= 1; i) {
+                zeros += Math.floor(number / i) / array.length ;
+                i = i * array[1];
             }
     }
-
-
-
-    return zeros;
+    return Math.floor(zeros);
 }
